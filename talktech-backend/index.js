0a84902e8cb8070ed295f295775e8e5752b4c22a
unknown <cruzyumari2@gmail.com>
2022-03-14 09:34:50 -0500

    Antes de los keys

--- talktech-backend/index.js
+++ talktech-backend/index.js
@@ -1,45 +1,43 @@
 /* eslint-disable prettier/prettier */
 
+const { Server } = require('socket.io');
 
- const {Server} = require ('socket.io');
-
-const express= require ('express');
+const express = require('express');
 
 const app = express();
 
-const http = require ('http');
+const http = require('http');
 
-const cors = require ('cors');
+const cors = require('cors');
 
 app.use(cors());
 
-const server=http.createServer(app);
+const server = http.createServer(app);
 
-    const io = new Server(server,{
-        cors:{
-            origin:"http://localhost:3000",
-            methods:["GET","POST"],
-        }
-    })
-
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
 
 // app.use(express.static(__dirname,'./build'));
 
-        io.on('connection', (socket) => {
-        console.log('usuario conectado',socket.id);
-        socket.on("join_canal",(data)=>{
-            socket.join(data);
-            console.log("user con id: ",socket.id," unido al canal: ",data);
-        })
+io.on('connection', (socket) => {
+  console.log('usuario conectado', socket.id);
+  socket.on('join_canal', (data) => {
+    socket.join(data);
+    console.log('user con id: ', socket.id, ' unido al canal: ', data);
+  });
 
-        socket.on("send_message",(data)=>{
-        socket.to(data.canal).emit("receive_message",data)
-        });
+  socket.on('send_message', (data) => {
+    socket.to(data.canal).emit('receive_message', data);
+  });
 
-        socket.on("disconnect",() => {
-            console.log("user desconectado",socket.id);
-        });
-        });
-        server.listen(3001, () => {
-            console.log(`Servidor inicializado`)
-        })
+  socket.on('disconnect', () => {
+    console.log('user desconectado', socket.id);
+  });
+});
+server.listen(3001, () => {
+  console.log(`Servidor inicializado`);
+});

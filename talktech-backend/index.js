2ba1063e4819059f66d9fb5859563545a4d074e4
Milagros LC <evidal0710@ich.edu.pe>
2022-03-13 19:44:11 -0500

    chat ready with websocket.io

--- talktech-backend/index.js
+++ talktech-backend/index.js
@@ -15,17 +15,17 @@ app.use(cors());
 
 const server=http.createServer(app);
 
- const io = new Server(server,{
-     cors:{
-         origin:"http://localhost:3000",
-         methods:["GET","POST"],
-     }
- })
+    const io = new Server(server,{
+        cors:{
+            origin:"http://localhost:3000",
+            methods:["GET","POST"],
+        }
+    })
 
 
 // app.use(express.static(__dirname,'./build'));
 
-io.on('connection', (socket) => {
+        io.on('connection', (socket) => {
         console.log('usuario conectado',socket.id);
         socket.on("join_canal",(data)=>{
             socket.join(data);
@@ -33,12 +33,13 @@ io.on('connection', (socket) => {
         })
 
         socket.on("send_message",(data)=>{
-console.log(data);
+        socket.to(data.canal).emit("receive_message",data)
         });
-socket.on("disconnect",() => { 
-    console.log("user desconectado",socket.id);
-});
-}); 
-server.listen(3001, () => {
-    console.log(`Servidor inicializado`)
-})
+
+        socket.on("disconnect",() => {
+            console.log("user desconectado",socket.id);
+        });
+        });
+        server.listen(3001, () => {
+            console.log(`Servidor inicializado`)
+        })
--- talktech/src/App.js
+++ talktech/src/App.js
@@ -8,28 +8,39 @@ function App() {
 
 const [userName,setUserName]=useState("");
 const [canal,setCanal]=useState("");
+const [showChat,setShowChat]=useState(false);
 const joinCanal=()=>{
 if(userName!==""&&canal!==""){
-    socket.emit("join_canal",canal)
+    socket.emit("join_canal",canal);
+    setShowChat(true);
     }
 }
 return (
 
-    <div className = "App" >
-       <h4>TalkTech</h4>
-        <input type="text" placeholder="Nombre" onChange={(event)=>{
-        setUserName(event.target.value);
-                }
-            }
-        />
-    <input type="text" placeholder="CanalId" onChange={(event)=>{
-    setCanal(event.target.value);
-            }
-        }
-    />
-    <button onClick={joinCanal}>chatear</button>
-    <Chat socket={socket} userName={userName} canal={canal}/>
-    </div>
+        <div className="App">
+        {!showChat ? (
+            <div className="joinChatContainer">
+            <h3>Unirte al Chat</h3>
+            <input
+                type="text"
+                placeholder="Tu nombre"
+                onChange={(event) => {
+                setUserName(event.target.value);
+                }}
+            />
+            <input
+                type="text"
+                placeholder="Canal ID..."
+                onChange={(event) => {
+                setCanal(event.target.value);
+                }}
+            />
+            <button onClick={joinCanal}>Unirte a un canal</button>
+            </div>
+        ) : (
+            <Chat socket={socket} userName={userName} canal={canal} />
+        )}
+        </div>
     );
 }
 

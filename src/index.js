0dd306cd73a2d2b956ce25ce8472528ee23fa334
unknown <cruzyumari2@gmail.com>
2022-04-01 23:56:16 -0500

    add deploy with heroku

--- src/index.js
+++ src/index.js
@@ -9,7 +9,12 @@ const app = express();
 // middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+app.use(
+  cors({
+    origin: '*',
+    methods: ['GET', 'POST'],
+  })
+);
 // routes
 app.use(require('./routes/index'));
 
